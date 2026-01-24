c14ef48ce5f8be1a69cd6a06847807fd499351bc
Arina <arina07kz@gmail.com>
2026-01-24 10:08:42 +0500

    Initial commit

---
+++ LAB2/Lab_02/Lab_21/my-app/src/FragmentLayout.jsx
@@ -0,0 +1,19 @@
+function FragmentLayout() {
+  return (
+    <>
+      <header>
+        <h1>Голова</h1>
+      </header>
+
+      <main>
+        <h2>Тело</h2>
+      </main>
+
+      <footer>
+        <h3>Ноги</h3>
+      </footer>
+    </>
+  );
+}
+
+export default FragmentLayout;
